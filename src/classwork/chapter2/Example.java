553b24bab81374389fa87c19a2d263cd2ad4f407
albertshahbazyan <albertshahbazyann@gmail.com>
2026-08-10 22:38:04 +0400

    First Commit

--- src/classwork/chapter2/Example.java
+++ src/classwork/chapter2/Example.java
@@ -4,7 +4,7 @@ public class Example {
 
     public static void main (String[] arg){
         String name = "jon";
-        int age = 85;
+        int age = 84;
         System.out.println(Example2.getUserAccessClean(age, name));
     }
 }
